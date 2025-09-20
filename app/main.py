266e89c8e0517dfbe828f62c90d6ed6db04fd3fe
welnecker <janiofdr@gmail.com>
2025-09-20 10:39:53 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ MODELOS_OPENROUTER = [
     "deepseek/deepseek-chat-v3-0324",
     "openai/gpt-4o-search-preview",
     "qwen/qwen-max",
-    "anthracite-org/magnum-v2-72b",
+    "alibaba/tongyi-deepresearch-30b-a3b",
     "qwen/qwen-vl-max",
 ]
 
